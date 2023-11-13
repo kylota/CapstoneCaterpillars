2a994fa46b63d799bc32fe6e0a287ee4641e40bf
Ky Stewart <kylota@yahoo.com>
2023-11-13 00:50:20 -0600

    homeComponent updated.

--- frontend/src/components/homeComponent.js
+++ frontend/src/components/homeComponent.js
@@ -6,6 +6,7 @@ import '../css/index.css';
 
 function Home() {
     return (<div>
+        <img src="https://cdn.pixabay.com/photo/2018/01/02/18/51/laptop-3056847_1280.jpg" className="img" alt="in progress" />
     </div>
     );
 }
